ff9b0674df30a8cceebe43c18410f2d547ee045a
Yorumitu <c1357907792@163.com>
2023-10-07 01:39:39 +0800

    4. 接入日志框架logback

--- msgcenter-web/src/main/java/com/ram/msgcenter/web/TestController.java
+++ msgcenter-web/src/main/java/com/ram/msgcenter/web/TestController.java
@@ -1,5 +1,6 @@
 package com.ram.msgcenter.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
  * 测试接口
  */
 @RestController
+@Slf4j
 public class TestController {
     @RequestMapping("/test")
     private String test(){
+        System.out.println("sout: sout测试");
+        log.info("log：log测试");
         return "测试测试测试";
     }
 }
