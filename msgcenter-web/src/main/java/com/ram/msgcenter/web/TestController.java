ed442bc8791153ac056590aade1ba20493806add
Yorumitu <c1357907792@163.com>
2023-10-07 19:19:46 +0800

    6. 设计表结构、接入MySQL、持久层框架实现简单查询

--- msgcenter-web/src/main/java/com/ram/msgcenter/web/TestController.java
+++ msgcenter-web/src/main/java/com/ram/msgcenter/web/TestController.java
@@ -1,8 +1,15 @@
 package com.ram.msgcenter.web;
 
+import com.alibaba.fastjson.JSON;
+import com.ram.msgcenter.support.dao.MessageTemplateDao;
+import com.ram.msgcenter.support.domain.MessageTemplate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author ramxx
@@ -11,10 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class TestController {
+
+    @Autowired
+    private MessageTemplateDao messageTemplateDao;
+
     @RequestMapping("/test")
     private String test(){
         System.out.println("sout: sout测试");
         log.info("log：log测试");
         return "测试测试测试";
     }
+
+    @RequestMapping("/database")
+    private String testDataBase() {
+        List<MessageTemplate> list = messageTemplateDao.findAllByIsDeletedEquals(0, PageRequest.of(0, 10));
+        return JSON.toJSONString(list);
+    }
+
 }
